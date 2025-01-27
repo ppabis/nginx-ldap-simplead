0162229d9a076313f8ab5dd39681ca8f5e64a4b9
ppabis <piotr@pabis.eu>
2025-01-27 21:56:16 +0100

    SSM parameters

---
+++ ssm_params.tf
@@ -0,0 +1,24 @@
+resource "aws_ssm_parameter" "ad_admin_password" {
+  name        = "/nginx-ldap/ad-admin-password"
+  description = "Simple AD Administrator Passwort"
+  type        = "SecureString"
+  value       = random_password.directory_password.result
+}
+
+resource "aws_ssm_parameter" "ad_server_name" {
+  name        = "/nginx-ldap/ad-server-name"
+  description = "Simple AD server name"
+  type        = "String"
+  value       = aws_directory_service_directory.simple_ad.name
+}
+
+data "aws_directory_service_directory" "simple_ad" {
+  directory_id = aws_directory_service_directory.simple_ad.id
+}
+
+resource "aws_ssm_parameter" "ad_dns_ip" {
+  name        = "/nginx-ldap/ad-dns-ip"
+  description = "Simple AD first DNS server IP"
+  type        = "String"
+  value       = tolist(data.aws_directory_service_directory.simple_ad.dns_ip_addresses)[0]
+}
