b4f8659d89b644dcafd4d004585969a2d88f885a
ppabis <piotr@pabis.eu>
2025-01-27 22:00:46 +0100

    Optional domain

---
+++ dns.tf
@@ -0,0 +1,15 @@
+data "aws_route53_zone" "mydomain" {
+  count = var.domain_name != "" ? 1 : 0
+  name  = var.domain_name
+  #provider = aws.net
+}
+
+resource "aws_route53_record" "nginx_ldap_auth_service" {
+  count   = var.domain_name != "" ? 1 : 0
+  zone_id = data.aws_route53_zone.mydomain[0].zone_id
+  name    = "nginx.${var.domain_name}"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.ldap_web[0].public_ip]
+  #provider = aws.net
+}
