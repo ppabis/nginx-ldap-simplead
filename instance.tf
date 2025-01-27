2ac484e735cbd354230bbff8676a46ddacc9214b
ppabis <piotr@pabis.eu>
2025-01-27 21:58:12 +0100

    Add initial userdata to the instance

--- instance.tf
+++ instance.tf
@@ -68,6 +68,17 @@ resource "aws_instance" "ldap_web" {
   lifecycle {
     ignore_changes = [ ami ]
   }
+
+  user_data = <<-EOF
+  #!/bin/bash
+  yum install docker git -y
+  systemctl enable --now docker
+  curl -L \
+    "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" \
+    -o /usr/local/bin/docker-compose
+  chmod +x /usr/local/bin/docker-compose
+  EOF
+  
 }
 
 output "instance_ip" {
